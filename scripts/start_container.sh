a90adff0b6210fcabb761205efff77ef8c86dc63
Sherylin <104746922+SherylinR@users.noreply.github.com>
2024-12-14 18:27:53 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+# Set credentials
+DOCKER_USERNAME="sherylin"
+DOCKER_PASSWORD="Sherylin6!"
+DOCKER_REGISTRY="docker.io" # Leave empty for Docker Hub
+
+# Login to Docker
+echo "$DOCKER_PASSWORD" | docker login "$DOCKER_REGISTRY" --username "$DOCKER_USERNAME" --password-stdin
+
 # Pull the Docker image from Docker Hub
 docker pull sherylin/my-python-flask-service
 
