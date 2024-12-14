eb40e5822debd8b1684edab069c51c08da9cd9af
Sherylin <104746922+SherylinR@users.noreply.github.com>
2024-12-14 17:54:33 +0530

    add the files for code deploy

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull sherylin/my-python-flask-service
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 pull sherylin/my-python-flask-service
