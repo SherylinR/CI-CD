70aada7956f7c454e68bddc15a7f188575ffa2c5
Sherylin <104746922+SherylinR@users.noreply.github.com>
2024-12-14 18:41:44 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 # Pull the Docker image from Docker Hub
-sudo docker pull sherylin/my-python-flask-service
+docker pull sherylin/my-python-flask-service
 
 # Run the Docker image as a container
-sudo docker run -d -p 5000:5000 pull sherylin/my-python-flask-service
+docker run -d -p 5000:5000 sherylin/my-python-flask-service
